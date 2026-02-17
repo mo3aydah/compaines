396719a545a0920443ad685abede17d6cdc67a4e
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2026-02-17 14:53:44 +0300

    -

--- assets/js/companies-config.js
+++ assets/js/companies-config.js
@@ -3,13 +3,11 @@
  * Used by index (landing) and card-flow (canvas image, optional overrides).
  */
 window.COMPANIES_CONFIG = [
-  { id: '6deg',   image: 'assets/images/6d.png',       nameEn: '6 Degrees',   nameAr: '٦ درجات' },
   { id: 'burooj', image: 'assets/images/burooj.png',    nameEn: 'Burooj',      nameAr: 'بروج' },
   { id: 'buroojair', image: 'assets/images/Buroojair.png', nameEn: 'Burooj Air', nameAr: 'بروج إير' },
   { id: 'deets',  image: 'assets/images/deets.png',     nameEn: 'Deets',       nameAr: 'ديتس' },
   { id: 'ec',     image: 'assets/images/ec.png',       nameEn: 'EC',          nameAr: 'EC' },
-  { id: 'naqash', image: 'assets/images/naqash.png',   nameEn: 'Naqash',     nameAr: 'نقش' },
-  { id: 'pe',     image: 'assets/images/pe.png',       nameEn: 'PE',          nameAr: 'PE' }
+  { id: 'naqash', image: 'assets/images/naqash.png',   nameEn: 'Naqash',     nameAr: 'نقش' }
 ];
 
 /**
